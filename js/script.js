c8a87af26155f1e74702b2a9f9f32797947dc8f1
Joaquin Talice <joaquintalice@gmail.com>
2023-08-19 11:13:33 -0300

    work done

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Escribe tu solución aquí
+  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+
+  function compareFn(a, b) {
+    let elem1 = a.toLowerCase();
+    let elem2 = b.toLowerCase();
+
+    if (elem1 < elem2) {
+      return -1;
+    } else if (elem1 > elem2) {
+      return 1;
+    }
+    // a must be equal to b
+    return 0;
+  }
+
+
+  const filteredArray = strangeArray.filter(element => typeof element === 'string');
+
+  const newArray = filteredArray.sort(compareFn);
+
+  showList(newArray)
+});
